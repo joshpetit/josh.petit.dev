838eba89361b332ea3d4b13cbe042fbc57b42c01
Joshua Petitma <joshua@petit.dev>
2022-12-09 15:04:03 -0500

    Make it better!

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,9 @@ import logo from "./logo.svg";
 import styles from "./App.module.css";
 
 function App() {
-  const [cats, setCats] = createSignal([
-    "Keyboard Cat",
-    "Maru",
-    "Henri The Existential Cat",
-  ]);
-
   return (
     <div class="bg-baseDark h-full text-center">
-      <Header></Header>
+      <Header />
     </div>
   );
 }
@@ -21,7 +15,7 @@ function Header() {
   return (
     <div class="bg-baseLight rounded-b-3xl p-5 flex flex-col items-center">
       <div class="flex bg-gray-300 h-40 rounded-full w-40 mb-5 web-cam">
-        <video class="object-cover" autoplay muted loop>
+        <video class="object-cover" poster="/src/assets/nice_bg.png"  muted autoplay loop>
           <source src="/src/assets/header.mp4" type="video/mp4" />
         </video>
       </div>
