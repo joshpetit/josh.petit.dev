77327b7a4c7cfb0d0d9e7f4b8fdec9f1c0793ce6
Joshua Petitma <joshua@petit.dev>
2022-12-09 15:50:57 -0500

    Try to get social icons to look good

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Component, createEffect, createSignal, For, Index } from "solid-js";
 
-import logo from "./logo.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
 import styles from "./App.module.css";
 
 function App() {
@@ -14,15 +15,33 @@ function App() {
 function Header() {
   return (
     <div class="bg-baseLight rounded-b-3xl p-5 flex flex-col items-center">
-      <div class="flex bg-gray-300 h-40 rounded-full w-40 mb-5 web-cam">
-        <video class="object-cover" poster="/src/assets/nice_bg.png"  muted autoplay loop>
+      <div class="flex bg-gray-300 h-40 rounded-full w-40 mb-4 web-cam">
+        <video
+          class="object-cover"
+          poster="/src/assets/nice_bg.png"
+          muted
+          autoplay
+          loop
+        >
           <source src="/src/assets/header.mp4" type="video/mp4" />
         </video>
       </div>
-      <p class="text-3xl">Joshua Petitma</p>
-      <p class="px-10">
-        Love God, work hard, help others, and experience life.
-      </p>
+      <div class="mb-4 px-6">
+        <p class="text-3xl">Joshua Petitma</p>
+        <p class="">Love God, work hard, help others, and experience life.</p>
+      </div>
+      <div class="flex w-full px-28 py-2 justify-evenly">
+        <SocialIcon icon={instagram}/>
+        <SocialIcon icon={linkedin} />
+      </div>
+    </div>
+  );
+}
+
+function SocialIcon(props: {icon: string}) {
+  return (
+    <div class="bg-black hover:bg-baseLight transition-colors w-fit overflow-visible rounded">
+      <img class="h-10 social-icon" src={props.icon} />
     </div>
   );
 }
